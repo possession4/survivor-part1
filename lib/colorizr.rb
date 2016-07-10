0112e7615d6a75a4a25eefa4632d41ca28154b8a
possession4 <himebrittny@hotmail.com>
2016-07-09 20:16:28 -0400

    Added colors, sample colors, and refactord code to include create_colors methods

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,41 +1,41 @@
 class String
+@@colors = [{ "name" => "red", "number" => "31"}, { "name" => "green", "number" => "32"}, 
+      { "name" => "yellow", "number" => "33"}, { "name" => "blue", "number" => "34"}, 
+      { "name" => "pink", "number" => "35"}, { "name" => "light_blue", "number" => "94"}, 
+      { "name" => "white", "number" => "97"}, { "name" => "light_grey", "number" => "37"}, 
+      { "name" => "black", "number" => "30"}]
 
-  def red
-    puts "\e[31m#{self}\e[0m"
-  end
-  
-  def green
-    puts "\e[32m#{self}\e[0m"
-  end
-
-  def yellow
-    puts "\e[33m#{self}\e[0m"
-  end
-
-  def blue
-    puts "\e[34m#{self}\e[0m"
-  end
-
-  def pink
-    puts "\e[35m#{self}\e[0m"
-  end
-
-  def light_blue
-    puts "\e[94m#{self}\e[0m"
+  def self.create_colors
+    @@colors.each do |color|
+      # new_color_method = %Q{
+      #           def #{color["name"]}
+      #               puts "\e[#{color["number"]}m\#{self}\e[0m"
+      #           end
+      #       }
+      #       class_eval(new_color_method)
+      self.send(:define_method, "#{color["name"]}") do
+        puts "\e[#{color["number"]}m#{self}\e[0m"
+      end
+    end
   end
 
-  def white
-    puts "\e[97m#{self}\e[0m"
+  def self.colors
+      colors_array = []
+      @@colors.each do |color|
+        colors_array.push(color["name"])
+      end
+      print colors_array
   end
 
-  def light_grey
-    puts "\e[30m#{self}\e[0m"
+  def self.sample_colors
+    @@colors.each do |color|
+         # puts "This is #{color}".send(color)
+         print "This is ".send("white").to_s.chomp + "#{color["name"]}".send(color["name"]).to_s.chomp
+    end 
   end
-
-  def black
-    puts "\e[31m#{self}\e[0m"
-  end
-
 end
 
+String.create_colors
+String.sample_colors
+String.colors
  
